a128a80fef152ebacf01bc413c50d518817b0b39
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-05-10 15:57:29 +0530

    src

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,7 @@ import logging
 import os
 from datetime import datetime
 
-
+os.makedirs('logs',exist_ok=True)
 logs_file = os.path.join('logs', f"{datetime.now():%d_%m_%H_%M_%S}.log")
 
 logging.basicConfig(
